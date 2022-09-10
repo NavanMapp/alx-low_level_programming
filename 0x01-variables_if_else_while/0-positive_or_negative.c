0023e7be8a8f9326d25f4246d50c59433ed1ec50
Your Name <you@example.com>
2022-09-09 18:14:49 -0700

    positive, negative, zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,17 +15,16 @@
  */
 int main(void)
 {
-	int n;
+int n;
 
-	srand(time(0));
-	n = srand() - RAND_MAX /2;
+srand(time(0));
+n = srand() - RAND_MAX /2;
 
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else id (n < 0)
-		print("%d is negative\n", n);
-	else
-		printf("%d is zero\n", n);
-
-	return(0);
+if (n > 0)
+printf("%d is positive\n", n);
+else id (n < 0)
+print("%d is negative\n", n);
+else
+printf("%d is zero\n", n);
+return(0);
 }
