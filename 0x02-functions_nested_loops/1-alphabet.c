3f3404dd49f8c33e01076431fef0197dacb6d216
Your Name <you@example.com>
2022-09-13 09:14:52 -0700

    lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,10 @@
-#include "main.h"
+#include <main.h>
 
 /**
 * alphabet- print alphabet in lower case
 *
 * return: always 0
 */
-int main(void)
-{
-print_alphabet();
-return(0);
-}
 void print_alphabet(void)
 {
 int letter;
