0435967358c9420986ccb195ebb9a44fb1587816
Your Name <you@example.com>
2022-09-13 13:32:41 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*
+* _abs: calculate the absolute value
+* @i: the integer to be converted
+* Return: absolute value
+*/
+
+int _abs(int i)
+{
+if (i < 0)
+i = i * -1;
+return (i);
+}
