78511b130797b509f760e66308b906c74120807f
Your Name <you@example.com>
2022-09-13 10:09:10 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,11 +3,12 @@
 *main - entry point
 * return: always 0(success)
 */
+
 int main(void)
 {
 char putchar[] = "_putchar";
 int i;
-for (i=0; i<9; i++)
+for (i=0; i<8; i++)
 {
 _putchar(putchar[i]);
 }
