d7daa23e39a591052f852f1199f7b40d625643aa
Your Name <you@example.com>
2022-09-13 10:14:36 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
-* main - entry point
+* main - prints the string "_putchar" from a character array
+*
 * return: always 0(success)
 */
 
@@ -8,7 +9,7 @@ int main(void)
 {
 char putchar[] = "_putchar";
 int i;
-for ( i = 0; i < 8; i++)
+for (i = 0; i < 8; i++)
 {
 _putchar(putchar[i]);
 }
