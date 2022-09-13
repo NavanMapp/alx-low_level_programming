dce751416194737e0a6d12cbe80146df2cedceab
Your Name <you@example.com>
2022-09-13 12:26:31 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,14 +2,14 @@
 
 /**
 * _islower - tests whether a character is a
-*
+* parameter or member 'c'
 * Return: 1 if lowercase and 0 if not lowercase
 */
 
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
-return(1);
+return (1);
 else
-return(0);
+return (0);
 }
