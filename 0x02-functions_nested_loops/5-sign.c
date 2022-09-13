edb8eb604bc6164d3c54960e9d242cba74744533
Your Name <you@example.com>
2022-09-13 13:16:21 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,19 +2,15 @@
 
 /**
 * @c: function that prints a sign of a number
-* Return: 0, 1
+* Return: 0, 1 and -1 will be returned
 */
 
 int print_sign(int n);
 {
 if (n > 0)
-{
 return (1);
-}
 else if (n = 0)
-{
 return (0);
-}
 else
 return (-1);
 }
