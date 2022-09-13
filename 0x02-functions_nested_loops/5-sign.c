6a4b5df86ee96ab0c4ebb6d7d6205015e0fe63b5
Your Name <you@example.com>
2022-09-13 13:11:45 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* @c: function that prints a sign of a number
+* Return: 0, 1
+*/
+
+int print_sign(int n);
+{
+if (n < 0)
+{
+return (1);
+}
+else if (n = 0)
+{
+retun (0);
+}
+else
+return (-1);
+}
