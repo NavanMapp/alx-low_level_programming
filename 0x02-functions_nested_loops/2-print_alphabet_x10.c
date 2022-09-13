b6c2e738f6b2e686fde5a4e50cff362707ed69c6
Your Name <you@example.com>
2022-09-13 11:57:05 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,13 +10,13 @@
 void print_alphabet_10x(void)
 {
   int i;
-  char c;
   for (i = 0; i < 10; ++i)
     {
-      for (c = 'a'; c <= 'z'; ++c)
+      char c;
+      for (c = 'a'; c <= 'z'; c++)
 	{
 	  _putchar(c);
 	}
-_putchar('\n');
     }
+  _putchar('\n');
 }
