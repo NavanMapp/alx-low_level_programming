4bf14764b417e99d2b375f8f1b06557063c1e451
NavanMapp <navanmapp@gmail.com>
2022-09-15 14:11:51 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+* mul - multiply two numbers
+* @a : first variable
+* @b: second variable
+* Return: results of the two
+*/
+int mul(int a, int b)
+}
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+* print_numbers - prints numbers from 0 - 9
+* Return: 0
+*/
+void print_numbers(void)
+{
+int i;
+for (i = 0; i < 9; i++)
+{
+printf("%d/n", i);
+}
+return (0);
+}
