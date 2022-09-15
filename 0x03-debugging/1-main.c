09beaa5f8e4d49f8dfdda23c7097708d391a35e5
Your Name <you@example.com>
2022-09-15 12:15:20 -0700

    2-largest_number.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,7 +13,8 @@ i = 0;
 *while (i < 10)
 *{
 *putchar(i);
-*} */
+*}
+*/
 printf("Infinite loop avoided! \\0/\n");
 return (0);
 }
--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,17 +10,17 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > a && b > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-    return (largest);
+if (a > b && a > c)
+{
+largest = a;
+}
+else if (b > c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
+return (largest);
 }
