0bbfd57ace5e0411732c6cd97c800bea38ce376d
Your Name <you@example.com>
2022-09-15 13:19:48 -0700

    main.h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+* main - check if letter is uppercase
+* Result: 0
+*/
+int _isupper(int c)
+{
+char c;
+ if (c = 'A' && c = 'Z')
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+/**
+* main - header file for prototypes
+* void prtotypes(void)
+* int prototypes(int)
+*/
+
+int _putchar(char);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif /* MAIN_H*/
