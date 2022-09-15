d4d7dddef996a16569751579f713dff48b123375
NavanMapp <navanmapp@gmail.com>
2022-09-15 14:42:17 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_line - prints line
+* @n: number of times
+* Return: 0
+*/
+void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 1; i <= i; i++)
+{
+_putchar(95);
+}
+_putchar('\n');
+}
