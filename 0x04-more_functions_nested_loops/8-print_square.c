258bee234959378772fa94d3c8d1646aa324914d
NavanMapp <navanmapp@gmail.com>
2022-09-15 15:08:58 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_square - square will be displayed
+* @size: variable
+* Return: 0
+*/
+void print_square(int size)
+{
+int x, y;
+if (size > 0)
+{
+for (x = 1; x <= size; x++)
+{
+for (y = 1; y <= size; y++
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}\
+}
