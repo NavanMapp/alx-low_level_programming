8fc19602bf3c31242ce765896fb601ab5ba4971d
Your Name <you@example.com>
2022-09-19 11:55:42 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,19 +1,19 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
-* _puts - prints string
-* @str: string input
+*_puts - prints a string*
+* @str: string prints
 * Description: prints a string
 * On success: returns no error
 */
-
 void _puts(char *str)
 {
 int i = 0;
-while (*(str[i]) != '\0')
+while (*(str + i) != '\0')
 {
-_putchar(*(str + i));
-i++
+putchar(*(str + i));
+i++;
 }
-_putchar(10);
+putchar(10);
 }
