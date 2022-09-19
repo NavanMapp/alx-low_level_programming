c0e4cfc14fe51a88427ccbb5e24af68687e3fb1c
Your Name <you@example.com>
2022-09-19 11:33:51 -0700

    commit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#ifdef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 
 /**
@@ -16,4 +16,6 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _atoi(char *str);
 
+#endif /* MAIN_H */
