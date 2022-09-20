179a769c80e1a91e34c1c9a4da7be7b0a3287e6f
Your Name <you@example.com>
2022-09-19 20:12:11 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* _atoi - converts string to integer
+* @s: string character
+* Return: 0;
+*/
+
+int _atoi(char *s)
+{
+int i;
+i = atoi(*(s + i));
+
+return (0);
+}
