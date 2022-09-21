1db16a240301d4a4b501ac4593f661b1d3cef8b7
NavanMapp <navanmapp@gmail.com>
2022-09-21 00:16:46 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _strcat - concatenates two strings
+ * @*dest: string 1
+ * @*src: string 2
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src){
+    int length = 0, i;
+    
+    while (dest[length]){
+        dest++;
+    }
+    for (i = 0; i < src[i]; i++){
+        dest = src[i];
+        length++;
+    }
+    
+    dest[length] = '\0';
+    resturn(dest);
+}
