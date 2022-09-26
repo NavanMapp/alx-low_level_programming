7e13abb34e75cdf45e2e1c17624a07d7a7a10f84
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-09-26 21:55:20 +0200

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-**
+/**
  * _strchr - locates a character in a string
  * @s: pointer that contains the address of the string
  * @c: character that needs to be located with in the string
@@ -10,7 +10,6 @@
 char *_strchr(char *s, char c)
 {
 	char *i = s;
-
 	while (*i != 0 && *i != c)
 	{
 		i++;
