bc4d2b39fe65e061d590453786f14d262378bb95
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-09-26 21:59:40 +0200

    Update 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* strpbrk - searches for any set of bytes
+* @s: pointer that contains the address of the string to be evaluated
+* @accept: pointer that contains a string
+* Return: address of the first occurrence of accept in the string
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s != 0)
+	{
+		for (i = 0; accept[i] != 0; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (0);
+}
