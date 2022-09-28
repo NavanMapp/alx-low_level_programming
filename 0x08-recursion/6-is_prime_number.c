7a2267021171bd225951c2f45fee40b7ec09971e
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-09-28 20:22:52 +0200

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,12 +5,6 @@
 * prime - helper function to is_prime_number
 * @a: variable in prime function
 * @b: variable in prime function
-*
-* is_prime_number - determines if number is a prime number
-* @n: number variable
-* Return: 1 if its prime number, 0 if its not
-*/
-
 int prime(int a, int b)
 {
 	if (a == b)
@@ -19,6 +13,12 @@ int prime(int a, int b)
 		return (0);
 	return (prime(a, b + 1));
 }
+
+/**
+* is_prime_number - determines if number is a prime number
+* @n: number variable
+* Return: 1 if its prime number, 0 if its not
+*/
 
 int is_prime_number(int n)
 {
