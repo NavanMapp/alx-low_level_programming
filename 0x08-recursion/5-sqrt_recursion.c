da59f4f51df8358030884c9412e8a16b29a6f5b0
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-09-28 20:28:55 +0200

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,6 +2,22 @@
 #include <math.h>
 
 /**
+* sqrt2 - helper function
+* @a: variable
+* @b: variable
+* Return: 0;
+*/
+
+int sqrt2(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	return (sqrt2(a, b + 1));
+}
+
+/**
 * _sqrt_recursion - function for square root
 * @n: number variable
 * Return: -1 if not natural square root, answer if square root found
@@ -9,6 +25,5 @@
 
 int _sqrt_recursion(int n)
 {
-	if (sqrt(n) != 0)
-		return (-1);
-	return (
+	return (sqrt2(n, 1));
+}
