50e10000c88ce73e55aedaf74f865052b3e6cead
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-09-28 19:03:33 +0200

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,9 +7,14 @@
 * Return: 0;
 */
 
-int _pow_recursion(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	return (x * _pow_recursion(pow(x, y - 1)));
+int _pow_recursion(int x, int y){
+ 
+    if (y < 0)
+        return -1;
+    else if (y == 0)
+        return 1;
+    else if (y % 2 == 0)
+        return _pow_recursion(x, y / 2) * _pow_recursion(x, y / 2);
+    else
+        return x * _pow_recursion(x, y / 2) * _pow_recursion(x, y / 2);
 }
