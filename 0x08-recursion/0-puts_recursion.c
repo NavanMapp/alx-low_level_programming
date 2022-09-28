25f0a5ddf3bdd25e17e06be13c723904acc49e16
Your Name <you@example.com>
2022-09-28 01:02:40 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _puts_recursion - print a string and a new line
+* @s: variable
+* Return: 0;
+*/
+
+void _puts_recursion(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		printf("%s \n", s);
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+* int prototype
+* char prototype
+* void prototype
+*/
+int _putchar(char c);
+int is_prime_number(int n);
+int _sqrt_recursion(int n);
+int _pow_recursion(int x, int y);
+int factorial(int n);
+int _strlen_recursion(char *s);
+void _print_rev_recursion(char *s);
+void _puts_recursion(char *s);
+
+#endif /* MAIN_H */
