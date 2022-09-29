c6e8e813623975d53d32a469465939688bc6623b
Your Name <you@example.com>
2022-09-29 06:57:25 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* is_palindrome - checks if string is palindrome
+* @s: string input
+* Return: 1 if string is palindrome, 0 if not
+*/
+
+int is_palindrome(char *s)
+{
+
---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+* wildcmp - compares two strings using recursion
+* @s1: variable
+* @s2: variable 2
+* Return: 1 if the strings are identical, 0 if otherwise
+*/
+
+
