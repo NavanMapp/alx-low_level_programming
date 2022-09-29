fbc7e77ec20d827e46160fd72fe896edb55d2344
Your Name <you@example.com>
2022-09-29 08:12:57 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,12 +8,8 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-if (s[i] == '\0')
-{
-	_putchar("\n");
-}
-else
-_putchar(s[0]);
+if (*s == '\0')
+return;
+print("%c", s);
 _puts_recursion(s + 1);
 }
