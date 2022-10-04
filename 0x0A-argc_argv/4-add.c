644441fb4110cf6b58fdbeff5b1b2e9d9caaa225
Your Name <you@example.com>
2022-10-04 04:34:39 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - sum of two numbers
+  * @argc: counter
+  * @argv: character vector
+  * Return: sum of two numbers
+  */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc == 1)
+	{
+		return (0);
+		printf("\n");
+	}
+	else if (argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		b = 0;
+		for (a = 1; a < argc; a++)
+		{
+			b += atoi(argv[i]);
+		}
+		printf("%d\n", b);
+	}
+}
