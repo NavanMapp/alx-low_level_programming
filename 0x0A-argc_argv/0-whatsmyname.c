6bb8ca151268e5bf56f69024af71fbd2d491d3f1
Your Name <you@example.com>
2022-10-04 02:51:39 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,11 @@
 
 /**
 * main - program that prints its name
+* @argc: argument count
+* @argv: argument vector
 */
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-	print("%s\n" *argv[0]);
+	printf("%s\n" argv[0]);
 }
