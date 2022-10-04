7c37c3de4bbe209b390ebafbec6b7a498a43e2f2
Your Name <you@example.com>
2022-10-04 08:44:53 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+  * _strdup - duplicates string
+  * @str: string variable
+  * Return: NULL
+  */
+
+char *_strdup(char *str)
+{
+	int i;
+
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	i = strlen(str);
+
+	s = (char *) malloc(i + 1);
+	strcpy(s, str);
+	return (s);
+}
