e3f23c865e646b4daf0073336d50e0ef47caacaa
Your Name <you@example.com>
2022-10-04 07:57:33 -0700

    0-create_array.c

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,11 +5,13 @@
 #include <stdlib.h>
 
 /** prototypes*/
-_putchar.c;
+int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *create_array(unsigned int size, char c);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+char **strtow(char *str);
+char *argstostr(int ac, char **av);
 
 #endif /*MAIN_H*/
