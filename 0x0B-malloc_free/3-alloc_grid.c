a85ae74c7984e203f7c13957cd03d262301c5122
Your Name <you@example.com>
2022-10-04 11:58:18 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * alloc_grid - two dimensional array
+  * @width: variable
+  * @height: variable
+  * Return
+  */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j, *p = NULL;
+
+	if (width <= 0 || weight <=)
+		return (NULL);
+
+	p = (int *)malloc(width * height * sizeof(int));
+
+	for (i = 0; i < width * height; i++)
+		;
+	
+	return (p);
+}
