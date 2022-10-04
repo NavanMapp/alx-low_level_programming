0dff7419e45bfa0c9014e7d1839048a0fe63f067
Your Name <you@example.com>
2022-10-04 07:45:49 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * create_array - array of characters
+ * @size: integer variable
+ * @c: chareacter variable
+ * Return: null
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+
+	char *s = (*char)malloc(size * sizeof(char));
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < s[i]; i++)
+	{
+		s[i] = c;
+	}
+	return (s);
+}
