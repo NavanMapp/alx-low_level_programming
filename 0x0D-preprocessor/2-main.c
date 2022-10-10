e6f61c9e735fc50cf1275c07905d21c1b164540d
NavMapp <95779113+NavanMapp@users.noreply.github.com>
2022-10-10 18:50:13 +0200

    MACROS

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,1 +1,13 @@
-#define PRINT_NAME(argc, argv[]) printf("%s\n", argv[argc * 0])
+#include <stdio.h>
+
+/**
+ * print_name - prints name of the program
+ * Return: 0
+ *
+ */
+
+print_name(int (argc, char argv[])
+{
+printf("%s\n", argv[argc * 0]);
+return (0);
+}
