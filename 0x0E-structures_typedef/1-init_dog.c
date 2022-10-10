193a939d1ab847ec59da58851a824e30e321f25f
NavMapp <95779113+NavanMapp@users.noreply.github.com>
2022-10-10 08:50:30 +0200

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct initi_dog *dog;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * dog - struct definition
+ * @name: variable
+ * @age: variable
+ * @owner: variable of struct
+ * Return: 0
+ */
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
