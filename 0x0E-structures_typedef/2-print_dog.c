2ea09412c379fd23fda72abd185023e5c62f0103
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-11 21:30:31 +0200

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,21 +1,4 @@
-#include "main.h"
 #include "dog.h"
-/**
- * dog - struct definition
- * dog_t - struct variable
- * @name: variable
- * @age: variable
- * @owner: variable of struct
- * Return: 0
- */
-
-struct dog {
-	char *name;
-	float age;
-	char *owner;
-};
-
-typedef struct dog dog_t;
 /**
  * print_dog - function that prints struct dog
  * @d: dog variable
