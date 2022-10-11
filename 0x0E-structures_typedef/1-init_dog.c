a1a43420c291ffa030dce04235ee2a09ef20a601
NavanMapp <navanmapp@gmail.com>
2022-10-11 12:44:48 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "dog.h"
 
 /**
@@ -20,5 +21,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
+	}
+}
 	}
 }
