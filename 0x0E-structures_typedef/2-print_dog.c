4c15851e3fa9f3258ce649c2a963945b27015017
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-11 21:57:56 +0200

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,12 +6,10 @@
  */
 void print_dog(struct dog *d)
 {
-if (!d->name && !d->age && !d->owner)
+if (d)
 {
-printf("nil");
-}
-else
 printf("%s\n", (d->name) ? d->name : "(nil)");
 printf("%f\n", (d->age) ? d->age : 0);
 printf("%s\n", (d->owner) ? d->owner : "(nil)");
+}
 }
