89a957443a28417af2a6d452ae448a9caf61fef3
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-11 22:17:48 +0200

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,39 +9,13 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *_dog;
-	int name_l = 0, own_l = 0;
-
-	if (name != NULL && owner != NULL)
-	{
-		name_l = _strlen(name) + 1;
-		own_l = _strlen(owner) + 1;
-		_dog = malloc(sizeof(dog_t));
-
-		if (_dog == NULL)
-			return (NULL);
-
-		_dog->name = malloc(sizeof(char) * name_l);
-
-		if (_dog->name == NULL)
-		{
-			free(_dog);
-			return (NULL);
-		}
-
-		_dog->owner = malloc(sizeof(char) * own_l);
-
-		if (_dog->owner == NULL)
-		{
-			free(_dog->name);
-			free(_dog);
-			return (NULL);
-		}
-
-		_dog->name = _strcpy(_dog->name, name);
-		_dog->owner = _strcpy(_dog->owner, owner);
-		_dog->age = age;
-	}
-
+	struct dog_t *_dog;
+	
+	_dog = malloc(sizeof(struct dog_t));
+	if (_dog == NULL)
+		return (NULL);
+	_dog->name = name;
+	_dog->age = age;
+	_dog->owner = owner;
 	return (_dog);
 }
