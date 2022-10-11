e5f04dad6f52f547c3247ce7753f709f430ca8a9
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-11 22:22:55 +0200

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * dog - struct definition
  * dog_t - struct variable
@@ -30,4 +31,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /** MAIN_H */
+#endif /** DOG_H */
