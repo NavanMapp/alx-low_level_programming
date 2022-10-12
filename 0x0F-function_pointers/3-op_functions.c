1773355ab1245e6831e6b314cfc11f8ff55e499f
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-13 00:23:33 +0200

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -37,6 +37,11 @@ int op_mul(int a, int b); /** op_mul: returns the product of @a: and @b: */
  */
 int op_div(int a, int b); /** op_div: returns the result of the division of @a: by @b: */
 {
+    if (b == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
     return (a / b);
 }
 /**
@@ -47,5 +52,10 @@ int op_div(int a, int b); /** op_div: returns the result of the division of @a: 
  */
 int op_mod(int a, int b)
 {
+    if (b == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
     return (a % b);
 }
