680e51d1d692905e5eb7191ea9f5c71030381191
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-13 00:15:14 +0200

    3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,30 +1,50 @@
 #include "3-calc.h"
-
-/** op_add: returns the sum of @a: and @b: .. */
-int op_add(int a, int b); /** op_add: returns the sum of @a: and @b: .. */
+/**
+ * op_add - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
+int op_add(int a, int b);
 {
     return (a + b);
 }
-
-/** op_sub: returns the difference of @a: and @b: */
+/**
+ * op_sub - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
 int op_sub(int a, int b); /** op_sub: returns the difference of @a: and @b: */
 {
     return (a - b);
 }
-
-/** op_mul: returns the product of @a: and @b: */
+/**
+ * op_mul - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
 int op_mul(int a, int b); /** op_mul: returns the product of @a: and @b: */
 {
     return (a * b);
 }
-
-/** op_div: returns the result of the division of @a: by @b: */
+/**
+ * op_div - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
 int op_div(int a, int b); /** op_div: returns the result of the division of @a: by @b: */
 {
     return (a / b);
 }
-
-/** op_mod: returns the remainder of the division of @a: by @b: */
+/**
+ * op_mod - 5 functions
+ * @a: input one
+ * @b: input two
+ * Return: 0
+ */
 int op_mod(int a, int b)
 {
     return (a % b);
