5ec40c192b92fe6f2cdb2b122f853ad4a3a40306
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-14 04:03:21 +0200

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,6 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-#inlcude <stdio.h>
-#include <stdarg.h>
-
-/** prototypes */
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
