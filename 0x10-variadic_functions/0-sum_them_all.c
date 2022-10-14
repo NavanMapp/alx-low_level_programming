1102f8e9a36bd4c252bd160ead8c83f0fd1a2af7
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-14 04:01:59 +0200

    Update 0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,14 +8,20 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-    unsigned int i, sum = 0;
-    va_list ap;
-    va_start(ap, n);
+	va_list valist;
+	unsigned int i, sum = 0;
 
-    for (i = 0; i < n; i++)
-    {
-        sum += va_arg(ap, const unsigned int);
-    }
-    va_end (ap);
-    return (sum);
+	if (n == 0)
+		return (0);
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(valist, const unsigned int);
+	}
+
+	va_end(valist);
+
+	return (sum);
 }
