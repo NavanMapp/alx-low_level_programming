afb5b22b55d67821111b94f3bb40237cc6413123
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-14 04:23:01 +0200

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,14 +1,32 @@
-#include <stdio.h>
-
-void print_strings(const char *separator, const unsigned int n, ...)
+#include "variadic_functions.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_strings - Entry Point
+ * @separator: comma space
+ * @n: number of elements
+ * Return: void
  */
-int main(void)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
-    print_strings(", ", 2, "Jay", "Django");
-    return (0);
+	char *sep, *p;
+	unsigned int i;
+	va_list list;
+
+	if (!separator || *separator == 0)
+		return;
+	else
+		sep = (char *) separator;
+	va_start(list, n);
+
+	if (n > 0)
+		printf("%s", va_arg(list, char *));
+	for (i = 1; i < n; i++)
+	{
+		p = va_arg(list, char*);
+		if (!p)
+			p = "(nil)";
+		printf("%s%s", sep, p);
+	}
+	printf("\n");
+	va_end(list);
 }
