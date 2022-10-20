1416e9432ca05d6d4790b74a3371375bdde0ce05
NavanMapp <navanmapp@gmail.com>
2022-10-20 16:14:12 +0000

    singly_linked_lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void print_first() __attribute__((constructor));
+
+/**
+ * print_first - prints something before main function
+ * Return: 0
+ */
+
+void print_first()
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
