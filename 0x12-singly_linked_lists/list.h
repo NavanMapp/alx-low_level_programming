fb4374dbdb33fc5b1ff5a2df173fd20e6b267209
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-20 20:38:22 +0200

    Update list.h

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,9 +1,8 @@
-#ifndef LIST_H
-#define LIST_H
-
-#include <stdio.h>
+#ifndef LISTS
+#define LISTS
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -12,20 +11,18 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for alx  project
+ * for Holberton project
  */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}list_t;
+} list_t;
 
-int _strlen(const char *s);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
 #endif
