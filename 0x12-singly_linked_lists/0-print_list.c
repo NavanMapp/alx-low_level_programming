1a91a8f695b917ca15c41e23bdd139db89bffb0f
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-21 05:14:20 +0200

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,27 +1,20 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
-  * print_list - Prints all elements of a list
-  * @h: A linked list
-  *
-  * Return: The number of nodes
-  */
-size_t print_list(const list_t *h)
+ * list_len - return elements in a linked list
+ * @h: variable
+ *
+ * Return: 0
+ */
+
+size_t list_len(const list_t *h)
 {
-	size_t count = 0;
+ size_t i = 0;
 
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		h = h->next;
-		count++;
-	}
-
-	return (count);
+ while (h)
+ {
+  h = h->next;
+  i++;
+ }
+ return (i);
 }
