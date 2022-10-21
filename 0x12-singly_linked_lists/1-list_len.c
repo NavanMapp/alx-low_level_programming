db2023474286a57df66a0d2530075d4ffabbf9eb
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-21 05:05:39 +0200

    1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,22 @@
-#include "list.h"
+#include <stdlib.h>
 #include <stdio.h>
+#include "lists.h"
 
 /**
- * list_len - return elements in a linked list
- * @h: variable
- *
- * Return: 0
- */
-
+  * list_len - Show the number of elements of a list
+  * @h: A linked list
+  *
+  * Return: The number of elements of a list
+  */
 size_t list_len(const list_t *h)
 {
- size_t i = 0;
+	size_t count = 0;
 
- while (h)
- {
-  h = h->next;
-  i++;
- }
- return (i);
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
 }
