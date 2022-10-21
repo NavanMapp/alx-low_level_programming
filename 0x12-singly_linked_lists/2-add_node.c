8db22ca5f5c22a5911798121732c27ea1e85ce7c
NavanMapp <navanmapp@gmail.com>
2022-10-21 03:51:18 +0000

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,7 +1,28 @@
 #include "list.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
+/**
+ * print_list - struct that prints a list of elements
+ * @h: variable
+ *
+ * Return: 0
+ */
+
+size_t print_list(const list_t *h)
+{
+     size_t i = 0;
+
+     while (h)
+     {
+         if (h->str == NULL)
+         {
+             printf("[0] nil\n");
+         }
+         printf("[%d] %s\n", h->len, h->str);
+         h = h->next;
+         i++;
+     }
+     return (i);
+}
 
 /**
  * strdup - function to duplicate string
