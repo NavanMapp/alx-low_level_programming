4040132fc32fd6f765c9007de5b9c51da436f735
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-25 22:35:14 +0200

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -25,7 +25,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
     if (*head == NULL)
     {
         *head = node;
-        return (NULL);
+        return (*head);
     }
     else
     {
