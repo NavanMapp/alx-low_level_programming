2cf4cde2139cf5ac8ac808778e450e3c30c9a311
NavanMapp <navanmapp@gmail.com>
2022-10-25 17:47:15 +0000

    6-pop_listint.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -8,7 +8,7 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *node;, *end;
+    listint_t *node, *end;
 
     node = malloc(sizeof(listint_t));
 
@@ -16,12 +16,20 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
     {
         return (NULL);
     }
+
+    node->n = n;
+    node->next = NULL;
+
+    if (node == NULL)
+    {
+        return (NULL);
+    }
     else
     {
-        end = head;
-        while (end->next)
+        end = *head;
+        while (end->next != NULL)
             end = end->next;
         end->next = node;
     }
     return (*head);
-}    
+}
