3db63a0d519ac7a25b9ec58f3658ac19f6527db0
NavanMapp <navanmapp@gmail.com>
2022-10-25 13:39:27 -0700

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 
-/** dd_nodeint_end - function that adds a node at the end
+/**
+ * dd_nodeint_end - function that adds a node at the end
  * @head: variable
  * @n: variable
  * Return: new node
@@ -8,33 +9,33 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-    listint_t *node, *end;
-    
-    if (head)
-    {
-    node = malloc(sizeof(listint_t));
+	listint_t *node, *end;
 
-    if (node == NULL)
-    {
-        return (NULL);
-    }
+	if (head)
+	{
+		node = malloc(sizeof(listint_t));
 
-    node->n = n;
-    node->next = NULL;
+		if (node == NULL)
+		{
+			return (NULL);
+		}
 
-    if (*head == NULL)
-    {
-        *head = node;
-        return (*head);
-    }
-    else
-    {
-        end = *head;
-        while (end->next != NULL)
-            end = end->next;
-        end->next = node;
-        return (end);
-    }
-    }
-    return (NULL);
+		node->n = n;
+		node->next = NULL;
+
+		if (*head == NULL)
+		{
+			*head = node;
+			return (*head);
+		}
+		else
+		{
+			end = *head;
+			while (end->next != NULL)
+				end = end->next;
+			end->next = node;
+			return (end);
+		}
+	}
+	return (NULL);
 }
