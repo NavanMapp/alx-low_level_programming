c9b3ef3cb50e62e14eec4281024c1a85efe3febc
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-25 22:09:35 +0200

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -25,6 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
     {
         if (copy-next == NULL)
             return (-1);
+        n++;
         copy = copy->next;
     }
 
