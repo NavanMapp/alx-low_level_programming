5d80c7c3a7d12c40f638277917401594356f36e8
NavanMapp <navanmapp@gmail.com>
2022-10-26 17:54:01 +0000

    linked lists

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * reverse_listsint - function that returns a reverse linked list
+ * @head: variable
+ *
+ * Return: reversed list
+ */
+
+istint_t *reverse_listint(listint_t **head)
+{
+    listint_t *p, *n;
+
+    if (head)
+    {
+        while (*head)
+        {
+            next = *head;
+            *head = (*head)->next;
+            next->n = p;
+            p = n;
+        }
+        *head = p;
+        return (*head);
+    }
+    return (NULL);
+}
