13b89c382a96b58ee7fe091d33132d66a4e4ba8f
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-26 20:07:27 +0200

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -9,19 +9,21 @@
 
 istint_t *reverse_listint(listint_t **head)
 {
-    listint_t *prev = NULL, *next = NULL;
+    listint_t *h = NULL, *n = NULL;
 
 	if (head)
 	{
-		while (*head)
-		{
-			next = *head;
-			*head = (*head)->next;
-			next->next = prev;
-			prev = next;
-		}
-		*head = prev;
-		return (*head);
+		return (NULL);
 	}
-	return (NULL);
+	n = NULL;
+	
+	while ((*head)->next)
+	{
+		h = (*head)->next;
+		(*head)->next = n;
+		n = *head;
+		*head = h;
+	}
+	(*head)->next = n;
+	return (*head);
 }
