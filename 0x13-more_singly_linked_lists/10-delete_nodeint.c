32fca249d1b1333c3866b03a34fb5efa1045a783
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-26 17:41:19 +0200

    10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,17 +11,17 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int count = 1;
 	listint_t *new = *head, *temp;
-
+	
 	if (!head || !*head)
 		return (-1);
-
+	
 	if (index == 0)
 	{
 		*head = new->next;
 		free(new);
 		return (1);
 	}
-
+	
 	temp = *head;
 	while (temp)
 	{
@@ -32,20 +32,16 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			free(new);
 			return (1);
 		}
-
-<<<<<<< HEAD
-    for (n = 0; n < (index - 1); n++;)
+		
+ for (n = 0; n < (index - 1); n++;)
     {
         if (copy-next == NULL)
             return (-1);
 		n++;
         copy = copy->next;
     }
-=======
 		temp = temp->next;
 		count++;
 	}
->>>>>>> 8a48fdd388a14dccb7180e1f25a1d2e1a427c80c
-
 	return (-1);
 }
