dbf8c95ceffc0cf8a6c47ecf33ff73e274d8dd78
NavanMapp <navanmapp@gmail.com>
2022-10-27 05:59:52 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int value = 0;
+	unsigned int value = 0;
 
-    if (b == NULL)
-        return (0);
-    while (*b != '\0')
-    {
-        value = value << 1;
-        if (*b != '1' && *b != '0')
-            return (0);
-        else if (*b == '1')
-            value = value | 1;
-        b++;
-    }
-    return (value);
+	if (b == NULL)
+		return (0);
+
+	while (*b != '\0')
+	{
+		value = value << 1;
+		if (*b != '1' && *b != '0')
+			return (0);
+		else if (*b == '1')
+			value = value | 1;
+		b++;
+	}
+	return (value);
 }
