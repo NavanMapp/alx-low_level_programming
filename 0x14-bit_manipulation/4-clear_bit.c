baaf24ed723da529f9f86517c38ce4557bfd8c4f
NavanMapp <navanmapp@gmail.com>
2022-10-27 12:53:14 +0000

    binary

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - function that sets the value of a bit to 0 at a given index
+ * @n: number input
+ * @index: starting from 0 of bit you want to set
+ * Return: 1 if works, -1 if not
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+    int add;
+
+    if (index > 63)
+        return (-1);
+    add = 1 << index;
+    *n = *n & (~add);
+    return (1);
+}
