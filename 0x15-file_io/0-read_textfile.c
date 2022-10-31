9fbb87bad8a850fb725d0ca0726633175b44bd9f
Návan <95779113+NavanMapp@users.noreply.github.com>
2022-10-31 23:34:51 +0200

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,23 +9,29 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, readed;
-	char *buff = malloc(sizeof(char *) * letters);
+	ssize_t file, let, w;
+	char *text;
 
-	if (!buff)
+	text = malloc(letters);
+	if (text == NULL)
 		return (0);
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY, 0600);
-	if (fd == -1)
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+	{
+		free(text);
 		return (0);
+	}
 
-	readed = read(fd, buff, letters);
-	write(STDOUT_FILENO, buff, readed);
+	let = read(file, text, letters);
 
-	free(buff);
-	close(fd);
-	return (readed);
+	w = write(STDOUT_FILENO, text, let);
+
+	close(file);
+
+	return (w);
 }
