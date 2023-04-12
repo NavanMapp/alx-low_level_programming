32813078d1f64c5e50b5bb9df581eb4e3f4fdb20
NavanMapp <navanmapp@gmail.com>
2023-04-12 13:08:06 -0700

    0x1E-search_algorithms

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,33 +9,32 @@
  *
  * Return: index of the number
  */
- int recursive_search(int *array, size_t size, int value)
- {
-     size_t half = size / 2;
-     size_t i;
+int recursive_search(int *array, size_t size, int value)
+{
+	 size_t half = size / 2;
+	 size_t i;
 
-     if (array == NULL || size == 0)
-        return (-1);
-    printf("Searching in array");
+	 if (array == NULL || size == 0)
+		 return (-1);
+	 printf("Searching in array");
 
-    for (i = 0; i < size; i++)
-        printf("%s %d", (i == 0) ? ":" : ",", array[i]);
-    printf("\n");
+	 for (i = 0; i < size; i++)
+		 printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+	 printf("\n");
 
-    if (half && size % 2 == 0)
-        half--;
+	 if (half && size % 2 == 0)
+		 half--;
 
-    if (value == array[half])
-        return ((int)half);
+	 if (value == array[half])
+		 return ((int)half);
 
-    if (value < array[half])
-        return (recursive_search(array, half, value));
-    half++;
+	 if (value < array[half])
+		 return (recursive_search(array, half, value));
+	 half++;
+	 return (recursive_search(array + half, size - half, value) + half);
+}
 
-    return (recursive_search(array + half, size - half, value) + half);
- }
-
- /**
+/**
  * binary_search -  searches for a value in an array of integers
  * using the binary search algorithm
  * @array: a pointer to the first element of the array to search in
@@ -44,7 +43,7 @@
  *
  * Return: -1 if value is not present in array or if array is NULL
  */
- int binary_search(int *array, size_t size, int value)
+int binary_search(int *array, size_t size, int value)
 {
 	int index;
 
@@ -52,6 +51,5 @@
 
 	if (index >= 0 && array[index] != value)
 		return (-1);
-
 	return (index);
 }
